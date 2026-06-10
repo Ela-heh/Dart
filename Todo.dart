d7ad1294548ccd5f2364cdb724f6b2f77f5e66c4
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-10 18:15:13 +0330

    Create Todo.dart

---
+++ Todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  final String who;
+  final String what;
+  const Todo(this.who, this.what);
+}
+
+// Annotation applied to the function
+@Todo('Elaheh', 'what a ride!')
+void main(List<String> args) {
+  
+}
