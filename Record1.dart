df5782fac9a300a31a9dd2f42d60ea02542dcded
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-11 20:49:40 +0330

    Create Record1.dart

---
+++ Record1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  var record = ('HEllo', a: 2, 23, 'dkljkl');
+  print(record.a);
+  list(List<double> number) {
+    return (number.first, number.length);
+  }
+
+  print(list([2.3, 13, 35, 2.4]));
+}
