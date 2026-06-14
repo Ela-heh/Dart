0d9f85712c34268dd5c8ae746f7d4562e8b0d663
TheLastGoddess <ela.farajzadeh1382@gmail.com>
2026-06-14 17:15:15 +0330

    Create Crypto.dart

---
+++ Crypto.dart
@@ -0,0 +1,15 @@
+import "dart:math";
+import 'dart:convert';
+
+class Password {
+  static final Random _random = Random();
+
+  static String key([int length = 32]) {
+    List<int> values = List<int>.generate(length, (i) => _random.nextInt(256));
+    return base64Url.encode(values);
+  }
+}
+
+void main(List<String> args) {
+  print(Password.key());
+}
